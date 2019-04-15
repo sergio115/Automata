344b63a87d8901ed2ac013701f3f42357b8627ce
Sergio Eduardo Pérez Rosales <sergio.eddy25@hotmail.com>
2019-04-15 01:08:51 -0500

    Solo muestra el primer lexema
    Por el momento solo se esta trabajando con el if else del espacio para simplificar el automata con varios lexemas y uso de DataGridView.

--- Automata/Form1.Designer.cs
+++ Automata/Form1.Designer.cs
@@ -29,8 +29,13 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Fila = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lexema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Granema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,34 +48,66 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(636, 69);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Analizar";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(587, 188);
+            this.label1.Location = new System.Drawing.Point(594, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "El estado es: ";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Fila,
+            this.Columna,
+            this.Lexema,
+            this.Granema});
+            this.dataGridView1.Location = new System.Drawing.Point(563, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 394);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Fila
+            // 
+            this.Fila.HeaderText = "Fila";
+            this.Fila.Name = "Fila";
+            this.Fila.ReadOnly = true;
+            // 
+            // Columna
+            // 
+            this.Columna.HeaderText = "Columna";
+            this.Columna.Name = "Columna";
+            this.Columna.ReadOnly = true;
+            // 
+            // Lexema
+            // 
+            this.Lexema.HeaderText = "Lexema";
+            this.Lexema.Name = "Lexema";
+            this.Lexema.ReadOnly = true;
+            // 
+            // Granema
+            // 
+            this.Granema.HeaderText = "Granema";
+            this.Granema.Name = "Granema";
+            this.Granema.ReadOnly = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1012, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +116,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fila;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Columna;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lexema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Granema;
     }
 }
 
--- Automata/Form1.cs
+++ Automata/Form1.cs
@@ -15,6 +15,9 @@ namespace Automata
         int contador;
         char[] charsRead;
         bool aprobado;
+        int fila;
+        string lexema;
+        string granema;
         char[] letrasMinnusculas = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ',
                                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         char[] letrasMayusculas = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ',
@@ -24,56 +27,91 @@ namespace Automata
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
             charsRead = new char[textBox1.Text.Length];
             charsRead = textBox1.Text.ToCharArray();
+            dataGridView1.Rows.Clear();
 
             EstadoInicial();
             label1.Text = "El estado es: " + aprobado.ToString();
         }
 
-        public void EstadoInicial()
-        {
+        public void EstadoInicial() {
             contador = 0;
-            Estado1();
+            fila = dataGridView1.Rows.Add();
+            for (int i = 0; i < letrasMinnusculas.Length; i++) {
+                if ((contador < textBox1.Text.Length) && 
+                        (charsRead[contador] == letrasMinnusculas[i] || charsRead[contador] == letrasMayusculas[i])) {
+                    Estado1();
+                } else if ((contador < textBox1.Text.Length) && (charsRead[contador] == ' ')) {
+                    contador++;
+                    Estado110();
+                    dataGridView1.Rows.Add();
+                }
+
+                lexema = null;
+            }
         }
 
-        public void Estado1()
-        {
-            if (contador < textBox1.Text.Length)
-            {
-                if (charsRead[contador] == 'a')
-                {
+        #region Estados de Transición
+        #region Identificadores
+        public void Estado1() {
+            for (int i = 0; i < numeros.Length; i++) {
+                if ((contador < textBox1.Text.Length) && (charsRead[contador] == numeros[i])) {
+                    lexema += charsRead[contador].ToString();
                     contador++;
                     Estado1();
-                } else if (charsRead[contador] == 'b')
-                {
-                    contador++;
-                    Estado2();
+                }
+            }
+            for (int i = 0; i < letrasMinnusculas.Length; i++) {
+                if (contador < textBox1.Text.Length) {
+                    if (charsRead[contador] == letrasMinnusculas[i] || charsRead[contador] == letrasMayusculas[i]) {
+                        lexema += charsRead[contador].ToString();
+                        contador++;
+                        Estado1();
+                    } else if (charsRead[contador] == '_') {
+                        lexema += charsRead[contador].ToString();
+                        contador++;
+                        Estado2();
+                    } else {
+                        lexema += charsRead[contador].ToString();
+                        contador++;
+                        Estado100();
+                    }
                 }
             }
         }
+        #endregion
 
-        public void Estado2()
-        {
-            if (contador < textBox1.Text.Length)
-            {
-                if (charsRead[contador] == 'a')
-                {
-                    contador++;
-                    Estado2();
+        public void Estado2() {
+            for (int i = 0; i < numeros.Length; i++) {
+                    if ((contador < textBox1.Text.Length) && (charsRead[contador] == numeros[i])) {
+                        lexema += charsRead[contador].ToString();
+                        contador++;
+                        Estado1();
+                    }
                 }
-                else if (charsRead[contador] == 'b')
-                {
-                    contador++;
-                    Estado3();
-                }
+            for (int i = 0; i < letrasMinnusculas.Length; i++) {
+                if (contador < textBox1.Text.Length) {
+                    if ((contador < textBox1.Text.Length) &&
+                            (charsRead[contador] == letrasMinnusculas[i] || charsRead[contador] == letrasMayusculas[i])) {
+                        lexema += charsRead[contador].ToString();
+                        contador++;
+                        Estado1();
+                    } else {
+                        lexema += charsRead[contador].ToString();
+                        contador++;
+                        Estado501();
+                    }
+                }     
             }
         }
 
+        #region Estados no funcionales
         public void Estado3()
         {
             if (contador < textBox1.Text.Length)
@@ -110,10 +148,284 @@ namespace Automata
             }
         }
 
+        public void Estado5()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado6()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado7()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado8()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado9()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado10()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado11()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado12()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado13()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado14()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado15()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado16()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+
+        public void Estado17()
+        {
+            aprobado = true;
+
+            if (contador < textBox1.Text.Length)
+            {
+                if (charsRead[contador] == 'a')
+                {
+                    contador++;
+                    Estado4();
+                }
+                else if (charsRead[contador] == 'b')
+                {
+                    contador++;
+                    EstadoError();
+                }
+            }
+        }
+        #endregion
+        #endregion
+
+        #region Estados de Aceptación
+        public void Estado100() {
+            aprobado = true;
+            dataGridView1.Rows[fila].Cells[2].Value = lexema;
+            return;
+        }
+        public void Estado110()
+        {
+            aprobado = true;
+            lexema = "' '";
+            granema = "Espacio";
+            dataGridView1.Rows[fila].Cells[2].Value = lexema;
+            dataGridView1.Rows[fila].Cells[3].Value = granema;
+            return;
+        }
+        #endregion
+
+        #region Estados de Error
         public void EstadoError()
         {
             aprobado = false;
             return;
         }
+
+        public void Estado501() {
+            aprobado = false;
+            dataGridView1.Rows[fila].Cells[2].Value = lexema;
+            return;
+        }
+        #endregion
     }
 }
